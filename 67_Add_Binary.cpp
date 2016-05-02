395842a6094c0bfe79b018a12e883eef0f1bc0f4
archerdante <1413282843@qq.com>
2016-05-02 10:18:15 +0800

    67

--- 67_Add_Binary.cpp
+++ 67_Add_Binary.cpp
@@ -8,6 +8,30 @@ using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
+       	string big, small;
+        const int cc = 2;
+        if(a.size() > b.size())
+        {
+            big = a;
+            small = b;
+        }
+        else
+        {
+            big = b;
+            small = a;
+        }
+        int iC = 0;
+        int i = big.size() - 1;
+        int j = small.size() - 1;
+        while(i >= 0)
+        {
+            int t = (big[i] - '0') + (j>=0?small[j] - '0':0) + iC;
+            iC = t/cc;
+            big[i] = (t%cc) + '0';
+            i--;
+            j--;
+        }
+        if(iC > 0) big.insert(big.begin(),'0' + iC);
+        return big; 
     }
 };
