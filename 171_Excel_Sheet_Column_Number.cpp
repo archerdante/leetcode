9f6f9086ca7a92280b04131714377cb277ddf47e
archerdante <1413282843@qq.com>
2016-05-15 22:52:40 +0800

    171

---
+++ 171_Excel_Sheet_Column_Number.cpp
@@ -0,0 +1,26 @@
+#include <string>
+//Related to question Excel Sheet Column Title
+//
+//Given a column title as appear in an Excel sheet, return its corresponding column number.
+//
+//For example:
+//
+//    A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28 
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int ret = 0;
+        for(auto &a : s)
+        {
+            ret = ret * 26 + ((a - 'A') + 1);
+        }
+        return ret;
+    }
+};
+
