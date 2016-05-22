25e8dfbb0bc4c974566c641f6d397f26e705be1d
archerdante <1413282843@qq.com>
2016-05-22 09:48:21 +0800

    222 binary search

---
+++ 222_Count_Complete_Tree_Nodes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//Given a complete binary tree, count the number of nodes.
+//Definition of a complete binary tree from Wikipedia:
+//In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        int lh = 0, rh = 0;
+        TreeNode* ll = root;
+        TreeNode* rr = root;
+        while(ll) ++lh, ll = ll->left;
+        while(rr) ++rh, rr = rr->right;
+        if(lh == rh) return (1<<lh) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
