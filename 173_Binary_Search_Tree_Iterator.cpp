24e929deb6f2df1c018fc45bae2fe134f9c28217
archerdante <1413282843@qq.com>
2016-06-09 21:41:27 +0800

    173 stack

--- 173_Binary_Search_Tree_Iterator.cpp
+++ 173_Binary_Search_Tree_Iterator.cpp
@@ -4,3 +4,32 @@
 //Calling next() will return the next smallest number in the BST.
 //
 //Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+        searchLeft(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !nodeStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *node = nodeStack.top();
+        nodeStack.pop();
+        if(node->right) searchLeft(node->right); 
+        return node->val;
+    }
+private:
+    stack<TreeNode*> nodeStack;
+    void searchLeft(TreeNode* node)
+    {
+        while(node)
+        {
+            nodeStack.push(node);
+            node = node->left;
+        }
+    }
+};
